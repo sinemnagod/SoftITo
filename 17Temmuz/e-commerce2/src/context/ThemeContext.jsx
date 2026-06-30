0105bc7b60af41e414f0ce171e03c84982c91065
D2-07 <sinem121995@hotmail.com>
2026-06-30 15:51:08 +0300

    m

--- 17Temmuz/e-commerce2/src/context/ThemeContext.jsx
+++ 17Temmuz/e-commerce2/src/context/ThemeContext.jsx
@@ -2,22 +2,22 @@ import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export function ThemeProvider({children}) {
-    const [theme, setTheme] = useState('light')
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
 
-    const toggleTheme = () => {
-        const nextTheme = theme==='light'?'dark':'light';
-        setTheme (nextTheme);
-        if (nextTheme === "dark") {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    };
+  const toggleTheme = () => {
+    const nextTheme = theme === "light" ? "dark" : "light";
+    setTheme(nextTheme);
+    if (nextTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
-    return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
